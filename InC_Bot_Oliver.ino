faa937006c75f4bb2130900cfd159aab9c131f62
Oliver Xu <oxu@u.rochester.edu>
2024-04-01 23:43:55 -0400

    code patterns 21-53

--- InC_Bot_Oliver.ino
+++ InC_Bot_Oliver.ino
@@ -1,5 +1,6 @@
 /* IN C BOT 
-march 23 code for Oliver
+20240401 - Oliver Xu
+
 output coding - 
 - all 53 IN C modules
 - direct LED behavior for each to visualize the melodic modules
@@ -24,6 +25,7 @@ char LED_4 = 11;  // odd/even indicator of forward/back movement
 const int tuningStandard = 440;
 // in equal temperament, frequency of MIDI note number m is calculated with:
 // freq = 440 * 2^((m-69)/12)
+// https://homes.luddy.indiana.edu/donbyrd/Teach/MusicalPitchesTable.htm
 unsigned int G3_note = tuningStandard * pow(2.0, (55 - 69) / 12.0);
 unsigned int C4_note = tuningStandard * pow(2.0, (60 - 69) / 12.0);
 unsigned int E4_note = tuningStandard * pow(2.0, (64 - 69) / 12.0);
@@ -35,7 +37,12 @@ unsigned int Bb4_note = tuningStandard * pow(2.0, (70 - 69) / 12.0);
 unsigned int B4_note = tuningStandard * pow(2.0, (71 - 69) / 12.0);
 unsigned int C5_note = tuningStandard * pow(2.0, (72 - 69) / 12.0);
 unsigned int D5_note = tuningStandard * pow(2.0, (74 - 69) / 12.0);
+unsigned int E5_note = tuningStandard * pow(2.0, (76 - 69) / 12.0);
+unsigned int F5_note = tuningStandard * pow(2.0, (77 - 69) / 12.0);
+unsigned int Fs5_note = tuningStandard * pow(2.0, (78 - 69) / 12.0);
 unsigned int G5_note = tuningStandard * pow(2.0, (79 - 69) / 12.0);
+unsigned int A5_note = tuningStandard * pow(2.0, (81 - 69) / 12.0);
+unsigned int B5_note = tuningStandard * pow(2.0, (83 - 69) / 12.0);
 
 const int tempo = 120;
 double thirtySecondNoteDuration = 60000.0 / tempo / 8;
@@ -50,6 +57,7 @@ const int DOTTED_QUARTER = 12; // 750ms
 const int HALF = 16; // 1000ms
 const int DOTTED_HALF = 24; // 1500ms
 const int WHOLE = 32; // 2000ms
+const int DOTTED_WHOLE = 48; // 3000ms
 
 void playNote(unsigned int frequency, int rhythmicMultiplier, int led) {
   tone(8, frequency);
@@ -136,7 +144,7 @@ void setup() {
 void loop(){
   // call the melodies and just test as needed
   // beat(); // eight note pulse over everything
-  for (int i = 10; i <= 20; i++) {
+  for (int i = 1; i <= 53; i++) {
     // playPattern(i);
     // playPattern(i);
     // playPattern(i);
@@ -205,6 +213,105 @@ void playPattern(int num) {
       break;
     case 20:
       twenty();
+      break;
+    case 21:
+      twentyOne();
+      break;
+    case 22:
+      twentyTwo();
+      break;
+    case 23:
+      twentyThree();
+      break;
+    case 24:
+      twentyFour();
+      break;
+    case 25:
+      twentyFive();
+      break;
+    case 26:
+      twentySix();
+      break;
+    case 27:
+      twentySeven();
+      break;
+    case 28:
+      twentyEight();
+      break;
+    case 29:
+      twentyNine();
+      break;
+    case 30:
+      thirty();
+      break;
+    case 31:
+      thirtyOne();
+      break;
+    case 32:
+      thirtyTwo();
+      break;
+    case 33:
+      thirtyThree();
+      break;
+    case 34:
+      thirtyFour();
+      break;
+    case 35:
+      thirtyFive();
+      break;
+    case 36:
+      thirtySix();
+      break;
+    case 37:
+      thirtySeven();
+      break;
+    case 38:
+      thirtyEight();
+      break;
+    case 39:
+      thirtyNine();
+      break;
+    case 40:
+      forty();
+      break;
+    case 41:
+      fortyOne();
+      break;
+    case 42:
+      fortyTwo();
+      break;
+    case 43:
+      fortyThree();
+      break;
+    case 44:
+      fortyFour();
+      break;
+    case 45:
+      fortyFive();
+      break;
+    case 46:
+      fortySix();
+      break;
+    case 47:
+      fortySeven();
+      break;
+    case 48:
+      fortyEight();
+      break;
+    case 49:
+      fortyNine();
+      break;
+    case 50:
+      fifty();
+      break;
+    case 51:
+      fiftyOne();
+      break;
+    case 52:
+      fiftyTwo();
+      break;
+    case 53:
+      fiftyThree();
       break;
   }
 }
@@ -313,7 +420,7 @@ void thirteen() {
   playNote(F4_note, SIXTEENTH, 1);
   playNote(G4_note, EIGHTH, 2);
   playRest(DOTTED_EIGHTH);
-  // the written pattern has a 16th note tied to a dotted half note
+  // this is notated as a 16th note tied to a dotted half note
   playNote(G4_note, DOTTED_HALF, 2);
   playRest(SIXTEENTH);
 }
@@ -376,114 +483,299 @@ void twenty() {
   playNote(E4_note, SIXTEENTH, 1);
 }
 
-void twentySeven(){
-  
-    tone(8,E4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, HIGH); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-    tone(8,Fs4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, LOW); 
-      digitalWrite(LED_3, HIGH); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-
-    tone(8,E4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, HIGH); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-
-    tone(8,Fs4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, LOW); 
-      digitalWrite(LED_3, HIGH); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-
-
-    tone(8,G4_note);
-      digitalWrite(LED_1, HIGH); 
-      digitalWrite(LED_2, LOW); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, HIGH); 
-    delay(200);
-    OFF();
-    delay(50);
-
-
-    tone(8,E4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, HIGH); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-    tone(8,G4_note);
-      digitalWrite(LED_1, HIGH); 
-      digitalWrite(LED_2, LOW); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, HIGH); 
-    delay(75);
-    OFF();
-    delay(50);
-
-    tone(8,Fs4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, LOW); 
-      digitalWrite(LED_3, HIGH); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-    tone(8,E4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, HIGH); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-      tone(8,Fs4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, LOW); 
-      digitalWrite(LED_3, HIGH); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
-
-    tone(8,E4_note);
-      digitalWrite(LED_1, LOW); 
-      digitalWrite(LED_2, HIGH); 
-      digitalWrite(LED_3, LOW); 
-      digitalWrite(LED_4, LOW); 
-    delay(75);
-    OFF();
-    delay(50);
+void twentyOne() {
+  playNote(Fs4_note, DOTTED_HALF, 7);
 }
 
+void twentyTwo() {
+  playNote(E4_note, DOTTED_QUARTER, 1);
+  playNote(E4_note, DOTTED_QUARTER, 1);
+  playNote(E4_note, DOTTED_QUARTER, 1);
+  playNote(E4_note, DOTTED_QUARTER, 1);
+  playNote(E4_note, DOTTED_QUARTER, 1);
+  playNote(Fs4_note, DOTTED_QUARTER, 2);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(B4_note, EIGHTH, 5);
+}
 
+void twentyThree() {
+  playNote(E4_note, EIGHTH, 1);
+  playNote(Fs4_note, DOTTED_QUARTER, 2);
+  playNote(Fs4_note, DOTTED_QUARTER, 2);
+  playNote(Fs4_note, DOTTED_QUARTER, 2);
+  playNote(Fs4_note, DOTTED_QUARTER, 2);
+  playNote(Fs4_note, DOTTED_QUARTER, 2);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(B4_note, QUARTER, 5);
+}
 
+void twentyFour() {
+  playNote(E4_note, EIGHTH, 1);
+  playNote(Fs4_note, EIGHTH, 2);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(G4_note, DOTTED_QUARTER, 3);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(B4_note, EIGHTH, 5);
+}
 
-    
+void twentyFive() {
+  playNote(E4_note, EIGHTH, 1);
+  playNote(Fs4_note, EIGHTH, 2);
+  playNote(G4_note, EIGHTH, 3);  
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(A4_note, DOTTED_QUARTER, 4);
+  playNote(B4_note, DOTTED_QUARTER, 5);
+}
+
+void twentySix() {
+  playNote(E4_note, EIGHTH, 1);
+  playNote(Fs4_note, EIGHTH, 2);
+  playNote(G4_note, EIGHTH, 3);  
+  playNote(A4_note, EIGHTH, 4);  
+  playNote(B4_note, DOTTED_QUARTER, 5);
+  playNote(B4_note, DOTTED_QUARTER, 5);
+  playNote(B4_note, DOTTED_QUARTER, 5);
+  playNote(B4_note, DOTTED_QUARTER, 5);
+  playNote(B4_note, DOTTED_QUARTER, 5);
+}
+
+void twentySeven() {
+  playNote(E4_note, SIXTEENTH, 1);
+  playNote(Fs4_note, SIXTEENTH, 2);
+  playNote(E4_note, SIXTEENTH, 1);
+  playNote(Fs4_note, SIXTEENTH, 2);
+  playNote(G4_note, EIGHTH, 3);
+  playNote(E4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 3);
+  playNote(Fs4_note, SIXTEENTH, 2);
+  playNote(E4_note, SIXTEENTH, 1);
+  playNote(Fs4_note, SIXTEENTH, 2);
+  playNote(E4_note, SIXTEENTH, 1);
+}
+
+void twentyEight() {
+  playNote(E4_note, SIXTEENTH, 1);
+  playNote(Fs4_note, SIXTEENTH, 2);
+  playNote(E4_note, SIXTEENTH, 1);
+  playNote(Fs4_note, SIXTEENTH, 2);
+  playNote(E4_note, DOTTED_EIGHTH, 1);
+  playNote(E4_note, SIXTEENTH, 1);
+}
+
+void twentyNine() {
+  playNote(E4_note, DOTTED_HALF, 1);
+  playNote(G4_note, DOTTED_HALF, 2);
+  playNote(C5_note, DOTTED_HALF, 3);
+}
+
+void thirty() {
+  playNote(C5_note, DOTTED_WHOLE, 7);
+}
+
+void thirtyOne() {
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+}
+
+void thirtyTwo() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(F4_note, DOTTED_HALF, 1);
+  playRest(SIXTEENTH);
+  playNote(G4_note, DOTTED_QUARTER, 2);
+}
+
+void thirtyThree() {
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(F4_note, SIXTEENTH, 1);
+  playRest(EIGHTH);
+}
+
+void thirtyFour() {
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(F4_note, SIXTEENTH, 1);
+}
+
+void thirtyFive() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playRest(EIGHTH);
+  playRest(QUARTER);
+  playRest(QUARTER);
+  playRest(QUARTER);
+  playNote(Bb4_note, QUARTER, 4);
+  playNote(G5_note, DOTTED_HALF, 5);
+  playNote(A5_note, EIGHTH, 6);
+  playNote(G5_note, QUARTER, 5);
+  playNote(B5_note, EIGHTH, 7);
+  playNote(A5_note, DOTTED_QUARTER, 6);
+  playNote(G5_note, EIGHTH, 5);
+  playNote(E5_note, DOTTED_HALF, 4);
+  playNote(G5_note, EIGHTH, 5);
+  // this is notated as an 8th note tied to a dotted half note
+  playNote(Fs5_note, DOTTED_HALF, 4);
+  playRest(EIGHTH);
+
+  playRest(QUARTER);
+  playRest(QUARTER);
+  playRest(EIGHTH);
+  // this is notated as an 8th note tied to a half note
+  playNote(E5_note, HALF, 4);
+  playRest(EIGHTH);
+
+  playNote(F5_note, DOTTED_HALF, 7);
+}
+
+void thirtySix() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+}
+
+void thirtySeven() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);  
+}
+
+void thirtyEight() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+}
+
+void thirtyNine() {
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(C5_note, SIXTEENTH, 4);
+}
+
+void forty() {
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(F4_note, SIXTEENTH, 1);
+}
+
+void fortyOne() {
+  playNote(B4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+}
+
+void fortyTwo() {
+  playNote(C5_note, HALF, 3);
+  playNote(B4_note, HALF, 2);
+  playNote(A4_note, HALF, 1);
+  playNote(C5_note, HALF, 3);
+}
+
+void fortyThree() {
+  playNote(F5_note, SIXTEENTH, 3);
+  playNote(E5_note, SIXTEENTH, 2);
+  playNote(F5_note, SIXTEENTH, 3);
+  playNote(E5_note, SIXTEENTH, 2);
+  playNote(E5_note, EIGHTH, 2);  
+  playNote(E5_note, EIGHTH, 2);  
+  playNote(E5_note, EIGHTH, 2);  
+  playNote(F5_note, SIXTEENTH, 3);
+  playNote(E5_note, SIXTEENTH, 2);
+}
+
+void fortyFour() {
+  playNote(F5_note, EIGHTH, 3);  
+  playNote(E5_note, QUARTER, 2);
+  playNote(E5_note, EIGHTH, 2);  
+  playNote(C5_note, QUARTER, 1);
+}
+
+void fortyFive() {
+  playNote(D5_note, QUARTER, 3);
+  playNote(D5_note, QUARTER, 3);
+  playNote(G4_note, QUARTER, 1);
+}
+
+void fortySix() {
+  playNote(G4_note, SIXTEENTH, 1);
+  playNote(D5_note, SIXTEENTH, 2);
+  playNote(E5_note, SIXTEENTH, 3);
+  playNote(D5_note, SIXTEENTH, 2);
+  playRest(EIGHTH);
+  playNote(G4_note, EIGHTH, 1);
+  playRest(EIGHTH);
+  playNote(G4_note, EIGHTH, 1);
+  playRest(EIGHTH);
+  playNote(G4_note, EIGHTH, 1);
+  playNote(G4_note, SIXTEENTH, 1);
+  playNote(D5_note, SIXTEENTH, 2);
+  playNote(E5_note, SIXTEENTH, 3);
+  playNote(D5_note, SIXTEENTH, 2);
+}
+
+void fortySeven() {
+  playNote(D5_note, SIXTEENTH, 2);
+  playNote(E5_note, SIXTEENTH, 3);
+  playNote(D5_note, EIGHTH, 2);
+}
+
+void fortyEight() {
+  playNote(G4_note, DOTTED_HALF, 5);
+  playNote(G4_note, HALF, 5);
+  playNote(F4_note, HALF, 4);
+  playNote(F4_note, QUARTER, 4);
+}
+
+void fortyNine() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(Bb4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(Bb4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+}
+
+void fifty() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+}
+
+void fiftyOne() {
+  playNote(F4_note, SIXTEENTH, 1);
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(Bb4_note, SIXTEENTH, 3);
+}
+
+void fiftyTwo() {
+  playNote(G4_note, SIXTEENTH, 2);
+  playNote(Bb4_note, SIXTEENTH, 3);
+}
+
+void fiftyThree() {
+  playNote(Bb4_note, SIXTEENTH, 3);
+  playNote(G4_note, SIXTEENTH, 2);
+}
